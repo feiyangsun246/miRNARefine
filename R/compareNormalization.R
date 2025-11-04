48058c687bfa5124a3b5419e9d33a134be78f163
Feiyang <feiyang.sun@mail.utoronto.ca>
2025-11-04 04:13:13 -0500

    initialize files for compareNormalizing

---
+++ R/compareNormalization.R
@@ -0,0 +1,8 @@
+#' Compare Normalization Methods for miRNA Data
+#'
+#' Applies multiple normalization methods (log2 transformation, z-score scaling,
+#' and quantile normalization) and compares their effects on miRNA expression
+#' data. Helps select the most appropriate normalization strategy for consistent
+#' and comparable expression levels.
+#'
+#'
